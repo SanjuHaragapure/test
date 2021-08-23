b2e662eee03ad392084c6a5a549af19dd5018401
Sanju <sharagapure@riverbed.com>
2021-08-23 22:22:54 +0530

    newChanges

--- src/App.js
+++ src/App.js
@@ -1,139 +1,148 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useRef} from 'react';
-import {SearchBar} from './SearchBar';
+import { useState, useRef } from 'react';
+import { SearchBar } from './SearchBar';
 function App() {
+	const personName = useRef('');
+	const personEmail = useRef('');
+	const [ personList, setPersonList ] = useState([]);
+	const [ personDetailsView, setpersonDetailsView ] = useState('');
+	const [ personEdit, setPersonEdit ] = useState({ id: '', name: '', email: '' });
 
-  const personName = useRef("");
-  const personEmail = useRef("");
+	function addPersonDetailsView() {
+		setpersonDetailsView('addView');
+	}
 
-  
-  const [personList, setPersonList] = useState([]);
-  
-  const [personDetailsView, setpersonDetailsView] = useState("");
-  const [personEdit, setPersonEdit] = useState({id:"", name:"", email:""});
+	function personEditView(person) {
+		console.log(person);
+		setpersonDetailsView('EditView');
+		setPersonEdit(person);
+	}
 
-  function addPersonDetailsView(){
-    setpersonDetailsView("addView");
-  }
+	function addPersonDetails() {
+		setPersonList([
+			...personList,
+			{ id: personList.length + 1, name: personName.current.value, email: personEmail.current.value }
+		]);
+		setpersonDetailsView('');
+	}
 
+	function editPersonDetails(person) {
+		console.log('person=', person);
+		let newPersonList = personList.reduce((listEle, ele) => {
+			if (!listEle) {
+				listEle = [];
+			}
+			if (ele.id != person.id) {
+				listEle.push(ele);
+			} else {
+				listEle.push(person);
+			}
+			return listEle;
+		}, []);
 
-  function personEditView(person){
-      console.log(person);
-      setpersonDetailsView("EditView");
-      setPersonEdit(person);
-  }
-  
+		setPersonList(newPersonList);
+		setpersonDetailsView('');
+	}
 
-  function addPersonDetails(){
-    setPersonList([...personList, {id:personList.length+1, name: personName.current.value, email: personEmail.current.value }]);
-    setpersonDetailsView("");
-  }
+	function getPersonList() {
+		return personList
+			.filter((person) => (currentSearch === '' ? true : person.name.includes(currentSearch)))
+			.map((person) => {
+				return (
+					<tr key={person.id} data-item={person} onClick={() => personEditView(person)}>
+						<td>{person.id}</td>
 
-  function editPersonDetails(person){
-    console.log("person=",person);
-    let newPersonList = personList.reduce((listEle, ele) =>
-        {  
-            if(!listEle){
-                listEle =[];
-            }
-            if(ele.id!=person.id){
-                listEle.push(ele);
-            }
-            else{
-                listEle.push(person);
-            }
-            return listEle;
-        }, []);
+						<td>{person.name}</td>
+						<td>{person.email}</td>
+					</tr>
+				);
+			});
+	}
 
-    setPersonList(newPersonList);
-    setpersonDetailsView("");
-  }
+	const [ currentSearch, setCurrentSearch ] = useState('');
+	function setSearch(value) {
+		setCurrentSearch(value);
+	}
 
+	return (
+		<div className="App">
+			<div>
+				<SearchBar onUserInput={setSearch} />
+				<button
+					style={{ marginTop: '10px' }}
+					type="button"
+					onClick={addPersonDetailsView}
+					className="btn btn-success pull-right"
+				>
+					Add
+				</button>
+			</div>
 
-  function getPersonList(){
-    return personList.filter(person => 
-        currentSearch === "" ? true: person.name.includes(currentSearch) ).map((person) => {
-        return (
-            <tr key={person.id} data-item={person} onClick={() => personEditView(person)}>
-            <td>
-            {person.id}
-          </td>
+			<div className="personView">
+				<div>
+					<table border="1">
+						<thead>
+							<tr>
+								<th>Id</th>
+								<th>Name</th>
 
-          <td  >
-          {person.name}
-          </td>
-          <td >
-          {person.email}
-          </td>
-          </tr>
-          
-        );
-    });
-  }
+								<th>Email id</th>
+							</tr>
+						</thead>
+						<tbody>{getPersonList()}</tbody>
+					</table>
+				</div>
 
-  const [currentSearch, setCurrentSearch] = useState("");
-  function setSearch(value){
-      setCurrentSearch(value);
-  }
-
-
-  return (
-    <div className="App">
-      <div>
-      <SearchBar onUserInput={setSearch} ></SearchBar>
-         <button style={{marginTop: "10px"}} type="button" onClick={addPersonDetailsView} className="btn btn-success pull-right">Add</button>
-      </div>
-      
-    <div className="personView">
-      <div>
-            
-            <table border="1">
-            <thead>
-             <tr>
-             <th>
-             Id
-             </th>
-             <th>
-             Name
-             </th>
-
-             <th>
-             Email id
-             </th>
-
-             </tr>
-
-            </thead>
-            <tbody>
-            {getPersonList()}
-            </tbody>
-            </table>
-        </div>
-
-
-        <div>
-           { personDetailsView == "addView" &&
-               <div>Add <div>id: {personList.length+1}</div>
-                    <div>name: <input type="text"  ref={personName} /></div>
-                    <div>email: <input type="text"  ref={personEmail} /></div>
-                    <button style={{marginTop: "10px"}} type="button" onClick={addPersonDetails} className="btn btn-success pull-right">Submit</button>
-               </div>
-           }
-           { personDetailsView == "EditView" &&
-               
-               <div>Edit
-                <div>id: {personEdit.id}</div>
-                    <div>name: <input type="text" defaultValue={personEdit.name} ref={personName}  /></div>
-                    <div>email: <input type="text" defaultValue={personEdit.email} ref={personEmail}    /></div>
-                    <button style={{marginTop: "10px"}} type="button" onClick={()=>editPersonDetails({id: personEdit.id, name: personName.current.value, email:personEmail.current.value })} className="btn btn-success pull-right">Submit</button>
-               </div>
-               
-           }
-        </div>
-        </div>
-    </div>
-  );
+				<div>
+					{personDetailsView == 'addView' && (
+						<div>
+							Add <div>id: {personList.length + 1}</div>
+							<div>
+								name: <input type="text" ref={personName} />
+							</div>
+							<div>
+								email: <input type="text" ref={personEmail} />
+							</div>
+							<button
+								style={{ marginTop: '10px' }}
+								type="button"
+								onClick={addPersonDetails}
+								className="btn btn-success pull-right"
+							>
+								Submit
+							</button>
+						</div>
+					)}
+					{personDetailsView == 'EditView' && (
+						<div>
+							Edit
+							<div>id: {personEdit.id}</div>
+							<div>
+								name: <input type="text" defaultValue={personEdit.name} ref={personName} />
+							</div>
+							<div>
+								email: <input type="text" defaultValue={personEdit.email} ref={personEmail} />
+							</div>
+							<button
+								style={{ marginTop: '10px' }}
+								type="button"
+								onClick={() =>
+									editPersonDetails({
+										id: personEdit.id,
+										name: personName.current.value,
+										email: personEmail.current.value
+									})}
+								className="btn btn-success pull-right"
+							>
+								Submit
+							</button>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,17 +1,15 @@
 import { useRef } from 'react';
 
-export function SearchBar(props){
-    const inputSearch = useRef("");
-    
-    function handleChange(e){
-        console.log(e.target.value);
-        props.onUserInput(e.target.value);
-    }
-      return (
-        <div style={{marginTop: "10px"}}>
-          Search :<input type="text"  ref={inputSearch} onChange={(e)=>handleChange(e)}/>
-        </div>
-  
-      );
-  
-  }
+export function SearchBar(props) {
+	const inputSearch = useRef('');
+
+	function handleChange(e) {
+		console.log(e.target.value);
+		props.onUserInput(e.target.value);
+	}
+	return (
+		<div style={{ marginTop: '10px' }}>
+			Search :<input type="text" ref={inputSearch} onChange={(e) => handleChange(e)} />
+		</div>
+	);
+}
